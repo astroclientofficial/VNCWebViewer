e4e58156b866f5313bd2a7428888a7e52dc75887
Adam Hilliard <adh636@gmail.com>
2017-06-29 12:53:33 -0400

    Simplify web viewer

--- server.js
+++ server.js
@@ -1,41 +1,19 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+var express = require("express");
+var http = require("http");
+var app = express();
+var server = http.createServer(app);
 
-// Get our API routes
-const api = require('./server/routes/api');
+var port = 9200;
+app.set("port", port);
 
-const app = express();
+app.use(express.static("public"));
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+server.listen(port);
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+server.on("listening", function() {
+  console.log("Listening on port: " + port);
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.on("error", function(error) {
+  console.log(error);
+});
